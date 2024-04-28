ae5e6d838cef893d860ee90dffdabf8df4861ab5
Jefersonalves <ferreira.jefersonn@gmail.com>
2024-04-27 21:11:03 -0300

    use dynamic task mapping to invoke lambda to multiple cities

--- orchestration/dags/invoke_lambda_ingestion_dag.py
+++ orchestration/dags/invoke_lambda_ingestion_dag.py
@@ -1,9 +1,28 @@
 from datetime import datetime
 
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.operators.lambda_function import (
     LambdaInvokeFunctionOperator,
 )
+
+
+def fetch_territories(date: str):
+    import json
+
+    territory_list = ["2704302", "1302603", "1600303", "1721000"]
+    data = [
+        json.dumps(
+            {
+                "date": date,
+                "territory_id": territory,
+                "bucket_name": "political-datalake-raw",
+            }
+        )
+        for territory in territory_list
+    ]
+    return data
+
 
 default_args = {
     "owner": "airflow",
@@ -21,12 +40,19 @@ with DAG(
     catchup=True,
 ) as dag:
 
-    invoke_lambda_function = LambdaInvokeFunctionOperator(
+    get_territories = PythonOperator(
+        task_id="get_territories",
+        python_callable=fetch_territories,
+        op_args=["{{ ds }}"],
+        provide_context=True,
+    )
+
+    # https://airflow.apache.org/docs/apache-airflow/stable/authoring-and-scheduling/dynamic-task-mapping.html
+    invoke_lambda_function = LambdaInvokeFunctionOperator.partial(
         task_id="invoke_lambda_function",
         function_name="ingestion_lambda",
         aws_conn_id="aws_default",
-        payload='{"date": "{{ ds }}", "territory_id": "2704302", "bucket_name": "political-datalake-raw"}',
         region_name="sa-east-1",
-    )
+    ).expand(payload=get_territories.output)
 
-    invoke_lambda_function
+    get_territories >> invoke_lambda_function
